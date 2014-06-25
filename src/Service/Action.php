e3982a362834598057053a466dddce0205e205da
Dan <daniel.messenger@mayden.co.uk>
2014-06-25 15:29:06 +0100

    Added action class to return task ID and message. User not implemented.

--- src/Service/Action.php
+++ src/Service/Action.php
@@ -6,16 +6,16 @@ class Action implements ActionInterface
 {
     public function taskId()
     {
-
+    	return $this->taskId;
     }
 
     public function userId();
     {
-
+    	throw new Exception('Not implemented');
     }
 
     public function message();
     {
-
+    	return $this->message();
     }
 }
