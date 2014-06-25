c4fcd5b48717679035e90a848b23f8740a94d0dc
Dan <daniel.messenger@mayden.co.uk>
2014-06-25 15:16:20 +0100

    Added Action.php and started implementing SpringLoopsStrategy

---
+++ src/Service/Action.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Service;
+
+class Action implements ActionInterface
+{
+    public function taskId()
+    {
+
+    }
+
+    public function userId();
+    {
+
+    }
+
+    public function message();
+    {
+
+    }
+}
--- src/Service/Parser/SpringLoopsStrategy.php
+++ src/Service/Parser/SpringLoopsStrategy.php
@@ -7,6 +7,12 @@ class SpringLoopsStrategy implements ParserStrategyInterface
 
     public function parse(array $data)
     {
+        $decoded = json_decode($data['payload']);
+        $message = $decoded['commitMessage'];
+        preg_match("/{{#TASK_ID ([0-9]+)}}/", $message, $matches);
+        $taskId = $matches[1];
+
+        $action = new Service\Action();
         
     }
 
